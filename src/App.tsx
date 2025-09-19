9535796edd79e1bace8c43d68ad9d42b05aacfec
Zeeshan <zh012948@gmail.com>
2025-09-19 23:20:39 +0500

    Hadith Generator using react Typescript and TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import HadithGenerator from "./components/HadithGenerator";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
     <>
       <HadithGenerator />
+      <Analytics />
     </>
   );
 }
